ead8231c165521fda419a792d59711ec76ae02dc
luckyhanye <404133902@qq.com>
2017-03-18 10:01:58 +0800

    react-tiger

---
+++ src/app.js
@@ -0,0 +1,21 @@
+import React from "react"
+
+
+class App extends React.Component{
+  constructor(){
+    super();
+    this.state={
+
+    }
+  }
+  render(){
+    return(
+      <div>
+        aaaa
+      </div>
+    )
+  }
+}
+
+
+export default App
